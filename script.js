1e741272a4280a54c42dbb52004c39c299893b46
Ana Vinhas <ana.vinhas@surforma.com>
2022-02-14 11:48:26 +0000

    adding starting project

---
+++ script.js
@@ -0,0 +1,27 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+
+function changeColor () {
+	body.style.background = 
+		"linear-gradient(to right, "+ color1.value + ", "+ color2.value +")";
+
+	css.textContent = body.style.background + ";";
+}
+
+function setBg() {
+	var randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+	var randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+
+	body.style.background = "linear-gradient(to right, #" + randomColor1 +", #" + randomColor2 +")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", changeColor);
+
+color2.addEventListener("input", changeColor);
+
+genNew.addEventListener("click", setBg);
